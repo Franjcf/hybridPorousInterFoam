50e2e9e463f01bd8eabeb7de92a859e9024f3c82
franjcf94 <franjcf@outlook.com>
2019-10-24 15:43:58 -0400

    changed naming convention

--- hybridMultiphaseFoamV7/tutorials/tutorialList.py
+++ hybridMultiphaseFoamV7/tutorials/tutorialList.py
@@ -3,26 +3,26 @@
 
 tutorials = [
 	     # Darcy Flow Validation
-             {'category' : "DarcyFlowCases", 'case' : "Buckley-Leverett/Flow_Driven_BrooksCorey"}, \
-             {'category' : "DarcyFlowCases", 'case' : "Buckley-Leverett/Gravity_Driven_BrooksCorey"}, \
-	     {'category' : "DarcyFlowCases", 'case' : "Buckley-Leverett/Flow_Driven_VanGenuchten"}, \
-             {'category' : "DarcyFlowCases", 'case' : "Buckley-Leverett/Gravity_Driven_VanGenuchten"}, \
-             {'category' : "DarcyFlowCases", 'case' : "GravityCapillarityBalance/Gravity_Capillarity_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Flow_Driven_BrooksCorey"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Gravity_Driven_BrooksCorey"}, \
+	     {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Flow_Driven_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Gravity_Driven_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/Gravity_Capillarity_VanGenuchten"}, \
             
 	     # Free-Fluid Validation          
-             {'category' : "FreeFlowCases", 'case' : "Bretherton/porousBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "Bretherton/standardBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "CapillaryRiseInTube/porousBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "CapillaryRiseInTube/standardBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "FlatPlateContactAngle/porousBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "FlatPlateContactAngle/standardBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "HeleShawCapillarFlow/porousBoundary"}, \
-             {'category' : "FreeFlowCases", 'case' : "HeleShawCapillarFlow/standardBoundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Bretherton/porous_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Bretherton/standard_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Capillary_Rise/porous_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Capillary_Rise/standard_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Flat_Plate_Contact_Angle/porous_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Flat_Plate_Contact_Angle/standard_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillar_Flow/porous_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillar_Flow/standard_Boundary"}, \
 
              # Example Applications
-             {'category' : "ExampleApplications", 'case' : "ViscousFingeringReservoir/viscousFingering"}, \
-            # {'category' : "ExampleApplications", 'case' : "CoastalBarrier/coastalBarrier"}, \
-            # {'category' : "ExampleApplications", 'case' : "Fracture/fracture"}, \
-             {'category' : "ExampleApplications", 'case' : "DrainageAndImbibition/imbibition"}, \
-             {'category' : "ExampleApplications", 'case' : "DrainageAndImbibition/drainage"}
+             {'category' : "Example_Applications", 'case' : "Viscous_Fingering_Reservoir/viscous_Fingering"}, \
+            # {'category' : "Example_Applications", 'case' : "Coastal_Barrier/coastalBarrier"}, \
+            # {'category' : "Example_Applications", 'case' : "Fracture/fracture"}, \
+             {'category' : "Example_Applications", 'case' : "Drainage_and_Imbibition/imbibition"}, \
+             {'category' : "Example_Applications", 'case' : "Drainage_and_Imbibition/drainage"}
             ]
