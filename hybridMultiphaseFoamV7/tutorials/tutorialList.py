90b54f47248457b8154978a21c582aecd1a876de
franjcf94 <franjcf@outlook.com>
2019-10-24 17:21:22 -0400

    added capillary rise case for brooks Corey

--- hybridMultiphaseFoamV7/tutorials/tutorialList.py
+++ hybridMultiphaseFoamV7/tutorials/tutorialList.py
@@ -8,6 +8,7 @@ tutorials = [
 	     {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Flow_Driven_VanGenuchten"}, \
              {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Gravity_Driven_VanGenuchten"}, \
              {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/Gravity_Capillarity_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/Gravity_Capillarity_BrooksCorey"}, \
             
 	     # Free-Fluid Validation          
              {'category' : "Free_Flow_Cases", 'case' : "Bretherton/porous_Boundary"}, \
@@ -16,8 +17,8 @@ tutorials = [
              {'category' : "Free_Flow_Cases", 'case' : "Capillary_Rise/standard_Boundary"}, \
              {'category' : "Free_Flow_Cases", 'case' : "Flat_Plate_Contact_Angle/porous_Boundary"}, \
              {'category' : "Free_Flow_Cases", 'case' : "Flat_Plate_Contact_Angle/standard_Boundary"}, \
-             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillar_Flow/porous_Boundary"}, \
-             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillar_Flow/standard_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillary_Flow/porous_Boundary"}, \
+             {'category' : "Free_Flow_Cases", 'case' : "Hele-Shaw_Capillary_Flow/standard_Boundary"}, \
 
              # Example Applications
              {'category' : "Example_Applications", 'case' : "Viscous_Fingering_Reservoir/viscous_Fingering"}, \
