e73b8c89bc4bf33144e5f1b19e55feae36663665
franjcf94 <franjcf@outlook.com>
2019-10-28 11:56:54 -0400

    added Fracture Case and corrected boundary names

--- hybridMultiphaseFoamV7/tutorials/tutorialList.py
+++ hybridMultiphaseFoamV7/tutorials/tutorialList.py
@@ -3,12 +3,12 @@
 
 tutorials = [
 	     # Darcy Flow Validation
-             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Flow_Driven_BrooksCorey"}, \
-             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Gravity_Driven_BrooksCorey"}, \
-	     {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Flow_Driven_VanGenuchten"}, \
-             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/Gravity_Driven_VanGenuchten"}, \
-             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/Gravity_Capillarity_VanGenuchten"}, \
-             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/Gravity_Capillarity_BrooksCorey"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/flow_Driven_BrooksCorey"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/gravity_Driven_BrooksCorey"}, \
+	     {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/flow_Driven_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Buckley-Leverett/gravity_Driven_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/gravity_Capillarity_VanGenuchten"}, \
+             {'category' : "Darcy_Flow_Cases", 'case' : "Gravity_Capillarity_Balance/gravity_Capillarity_BrooksCorey"}, \
             
 	     # Free-Fluid Validation          
              {'category' : "Free_Flow_Cases", 'case' : "Bretherton/porous_Boundary"}, \
@@ -23,7 +23,7 @@ tutorials = [
              # Example Applications
              {'category' : "Example_Applications", 'case' : "Viscous_Fingering_Reservoir/viscous_Fingering"}, \
             # {'category' : "Example_Applications", 'case' : "Coastal_Barrier/coastalBarrier"}, \
-            # {'category' : "Example_Applications", 'case' : "Fracture/fracture"}, \
+             {'category' : "Example_Applications", 'case' : "Fracture/fracture"}, \
              {'category' : "Example_Applications", 'case' : "Drainage_and_Imbibition/imbibition"}, \
              {'category' : "Example_Applications", 'case' : "Drainage_and_Imbibition/drainage"}
             ]
