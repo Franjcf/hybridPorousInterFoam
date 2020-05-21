ccd6917c36ee7024faaca2446bade94095c730f9
franjcf94 <franjcf@outlook.com>
2020-05-21 19:08:22 -0400

    removed unnecessary information from the tutorials

--- tutorials/Example_Applications/Viscous_Fingering_Reservoir/viscous_Fingering/viscousFingering.cmd
+++ tutorials/Example_Applications/Viscous_Fingering_Reservoir/viscous_Fingering/viscousFingering.cmd
@@ -1,13 +1,8 @@
 #!/bin/bash
-#parallel job using 4 proccesors and runs for 1 hour (max)
+#parallel job using 16 proccesors and runs for 12 hours (max)
 #SBATCH -N 1 # node count
 #SBATCH --ntasks-per-node=16 #number of threads desired
 #SBATCH -t 12:00:00
-# sends mail when process begins, and
-# when it ends. Make sure you define your email
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-user=fjc2@princeton.edu
 
 # Load openmpi environment
 module load openmpi/gcc
